c255b7753e43720299703c6367204e16e2b726ee
Petit Emmanuel <kwobapetit@gmail.com>
2020-12-15 11:47:52 +0300

    username validation but has an error

--- js/shifu.js
+++ js/shifu.js
@@ -1,3 +1,17 @@
 const changeColor = document.querySelector('.changeColor');
 
 Text.classList.add("change");
+
+/*login validation */
+function validate() {
+    let username = document.getElementById("name").value;
+    let password = document.getElementById("password").value;
+    if (username == null || username == "") {
+        alert("Please enter the username");
+        return false;
+    } else if  (password  == null ||  password  == "")  {                
+        alert("Please enter the password.");             
+        return  false;            
+    }            
+    alert('Login successful');
+}
